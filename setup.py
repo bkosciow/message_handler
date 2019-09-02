bd39cd0c2748bfb505a25c5c1c7da86bd64078e8
Bartosz Kościów <kosci1@gmail.com>
2019-09-02 18:08:38 +0200

    fix docs

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     version='1.0.0',
     description='Server listener and iot_message handler',
     keywords=['iot_message', 'raspberry pi'],
-    long_description=(read('readme.md')),
+    long_description=(read('README.rst')),
     url='https://github.com/bkosciow/message_listener',
     license='MIT',
     author='Bartosz Kościów',
